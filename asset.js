23aafbc55c0ab93be308e6081a7557c5db684dca
Nick Gorajski <nick_gorajski@bcbsil.com>
2020-02-25 15:15:03 -0600

    Remove cube, add spherical shell

--- asset.js
+++ asset.js
@@ -1,27 +1,32 @@
 let scene, camera, renderer
-let cube
+let sphere
 let increment = 0.1;
 
 window.onload = () => { 
 
-	let createCube = function() {
-		let geometry = new THREE.BoxGeometry(1,1,1)
-		let material = new THREE.MeshBasicMaterial({color: 0x369CDD})
-		cube = new THREE.Mesh(geometry, material)
-		scene.add(cube)
+	let createSphere = function() {
+		let geometry = new THREE.SphereGeometry(5,8,8,0,Math.PI,0,Math.PI/2)
+		// Sphere Properties:
+		// radius 5
+		// and horz and vert segments of 8
+		// horizontal sweep angle goes from 0 to PI (starts at (0,0,0), ends at (PI,0,0) thru xz plane)
+		// vertical sweep angle goes from 0 to PI/2 (starts at (0,PI/2,0), ends at (0,0,0) thru xy plane)
+		let material = new THREE.MeshBasicMaterial({color: 0xffffff, wireframe: true})
+		sphere = new THREE.Mesh(geometry, material)
+		scene.add(sphere)
 	}
 
 	let init = function() {
 		// create scene
 		scene = new THREE.Scene()
-		scene.background = new THREE.Color(0xffffee)
+		scene.background = new THREE.Color(0x000000)
 		
 		// create and locate the camera
 		camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000)
-		camera.position.z = 5
+		camera.position.z = 20
 
 		// create and locate the objects on the scene
-		createCube()
+		createSphere()
 
 		// create the renderer
 		renderer = new THREE.WebGLRenderer({ antialias: true })
@@ -31,11 +36,8 @@ window.onload = () => {
 	}
 
 	let mainLoop = function() {
-		// cube.position.x += increment
-		cube.rotation.y += increment
-
-		if(cube.position.x <= -3 || cube.position.x >= 3)
-			increment *= -1
+		sphere.rotation.x += increment
+		sphere.rotation.y += increment
 
 		// let axes = new THREE.AxesHelper( 5 )
 		// scene.add(axes)
