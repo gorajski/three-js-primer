6f565048d0a8a31386152400e280790e418d91ab
Nick Gorajski <nickgorajski@gmail.com>
2020-03-14 14:16:40 -0500

    Demo MeshNormalMaterial

--- asset.js
+++ asset.js
@@ -3,38 +3,27 @@ let scene, camera, renderer
 window.onload = () => { 
 	
 	let STEP = 0.1;
-	let cube1, cube2, plane
+	let shape
 
 	let createGeometry = function () {
-		let geometry = new THREE.BoxGeometry(5,5,5)
-		let material = new THREE.MeshBasicMaterial({color: 0xc9b92b})
+		// let geometry = new THREE.BoxGeometry(5,5,5)
+		// let geometry = new THREE.SphereGeometry(5, 30, 30)
+		let geometry = new THREE.TorusGeometry(5, 2, 10, 12)
+		// let material = new THREE.MeshBasicMaterial({color: 0xbbbbbb, wireframe: true})
+		let material = new THREE.MeshNormalMaterial() 
 
-		cube1 = new THREE.Mesh(geometry,material)
-		cube1.position.z = -6
-		cube1.position.y = -5
+		shape = new THREE.Mesh(geometry,material)
 
-		geometry = new THREE.BoxGeometry(5,5,5)
-		material = new THREE.MeshBasicMaterial({color: 0xff0040, transparent: true, opacity: 0.8})
+		// normals = new THREE.FaceNormalsHelper(sphere, 5)
 
-		cube2 = new THREE.Mesh(geometry, material)
-		cube2.position.z = 6
-		cube2.position.y = -5
-
-		geometry = new THREE.PlaneGeometry(1000,1000,50,50)
-		material = new THREE.MeshBasicMaterial({color: 0xa6f995, wireframe: true})
-
-		plane = new THREE.Mesh(geometry, material)
-		plane.rotation.x = Math.PI / 2
-		plane.position.y = -100
-
-		scene.add(cube1)
-		scene.add(cube2)
-		scene.add(plane)
+		scene.add(shape)
+		// scene.add(sphere)
+		// scene.add(normals)
 	}
 
 	let init = function() {
 		scene = new THREE.Scene()
-		scene.background = new THREE.Color(0x000000)
+		scene.background = new THREE.Color(0x1111ee)
 
 		camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 10, 2000)
 		camera.position.z = 20
@@ -48,9 +37,9 @@ window.onload = () => {
 	}
 
 	let mainLoop = function() {
-		cube1.position.x += STEP
-		cube2.position.x -= STEP
-		if(Math.abs(cube1.position.x) > 6) STEP *= -1
+		shape.rotation.x += STEP
+		shape.rotation.y += STEP
+		// normals.update()
 
 		renderer.render(scene, camera)
 		requestAnimationFrame(mainLoop)
