efb76c1669132862d623919bdf20b3a3299d0cb3
Nick Gorajski <nickgorajski@gmail.com>
2020-03-14 14:51:03 -0500

    Demo Line and Point materials

--- asset.js
+++ asset.js
@@ -2,33 +2,42 @@ let scene, camera, renderer
 
 window.onload = () => { 
 	
-	let STEP = 0.1;
+	let STEP = 0.06;
 	let cube, sphere
 
 	let createGeometry = function () {
-		let material = new THREE.MeshDepthMaterial()
-		let geometry = new THREE.BoxGeometry(3,2,4)
+		// let material = new THREE.LineBasicMaterial({color: 0xffffff, linewidth: 1})
+		// let material = new THREE.LineDashedMaterial({color: 0xffffff, linewidth: 1, dashSize: 2, gapSize: 1})
+		let material = new THREE.PointsMaterial({color: 0xffffff})
+		let geometry = new THREE.CylinderGeometry(3,2,4)
 
-		cube = new THREE.Mesh(geometry,material)
-		cube.position.z = -10
-		cube.position.x = -5
+		// cylinder = new THREE.Line(geometry,material)
+		cylinder = new THREE.Points(geometry,material)
+		cylinder.position.z = -10
+		cylinder.position.x = -5
 
-		geometry = new THREE.SphereGeometry(3,30,30)
+		// cylinder.computeLineDistances()		// uncomment for Line materials
 
-		sphere = new THREE.Mesh(geometry, material)
+
+		geometry = new THREE.SphereGeometry(3, 30, 30)
+
+		// sphere = new THREE.Line(geometry, material)
+		sphere = new THREE.Points(geometry, material)
 		sphere.position.z = 0
 		sphere.position.x = 5
 
-		scene.add(cube)
+		// sphere.computeLineDistances()		// uncomment for Line materials
+
+		scene.add(cylinder)
 		scene.add(sphere)
 	}
 
 	let init = function() {
 		scene = new THREE.Scene()
-		scene.background = new THREE.Color(0xffffff)
+		scene.background = new THREE.Color(0x1111ee)
 
 		camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000)
-		camera.position.z = 5
+		camera.position.z = 15
 
 		createGeometry()
 
@@ -39,10 +48,10 @@ window.onload = () => {
 	}
 
 	let mainLoop = function() {
-		cube.position.z += STEP
-		sphere.position.z -= STEP
-
-		if(cube.position.z >= 6 || cube.position.z <= -16) STEP *= -1
+		cylinder.rotation.x += STEP
+		cylinder.rotation.y += STEP
+		sphere.rotation.x += STEP
+		sphere.rotation.y += STEP
 
 		renderer.render(scene, camera)
 		requestAnimationFrame(mainLoop)
