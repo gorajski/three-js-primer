7889834f25cf2ee87d10d03d4829a03465eb881c
Nick Gorajski <nickgorajski@gmail.com>
2020-03-28 08:53:43 -0500

    Lighting - Add Hemisphere light

--- asset.js
+++ asset.js
@@ -3,41 +3,40 @@ let scene, camera, renderer
 window.onload = () => { 
 	
 	let STEP = 0.1
-	let cube, cone, plane, light
+	let cube, sphere, plane, light
 
 	let createGeometry = function () {
 		let geometry = new THREE.BoxGeometry(5,5,5)
-		let material = new THREE.MeshPhongMaterial({color: 0x0f1d89, shininess: 100, side: THREE.DoubleSide})
+		let material = new THREE.MeshPhongMaterial({color: 0xdff913, shininess: 100, side: THREE.DoubleSide})
 
 		cube = new THREE.Mesh(geometry,material)
-		cube.position.z = -6
-		cube.position.y = -5
-		cube.position.x = -6
+		cube.position.set(5,0,0)
+		
+		geometry = new THREE.SphereGeometry(5,30,30)
+		material = new THREE.MeshPhongMaterial({color: 0x66cdaa, shininess: 100, side: THREE.DoubleSide})
+		sphere = new THREE.Mesh(geometry, material)
+		sphere.position.set(-5,5,-5)
 
-		geometry = new THREE.ConeGeometry(3,4,20,1,true)
-		cone = new THREE.Mesh(geometry, material)
-		cone.position.x = 7
-		cone.position.y = -5
-
-		geometry = new THREE.PlaneGeometry(1000, 1000, 50, 50)
+		geometry = new THREE.BoxGeometry(2000, 1, 2000)
 		material = new THREE.MeshPhongMaterial({color: 0x693421, side: THREE.DoubleSide})
 		plane = new THREE.Mesh(geometry,material)
-		plane.rotation.x = Math.PI/2
-		plane.position.y = -100;
+		plane.position.y = -1;
 
 		scene.add(cube)
-		scene.add(cone)
+		scene.add(sphere)
 		scene.add(plane)
 	}
 
 	let init = function() {
 		scene = new THREE.Scene()
-		scene.background = new THREE.Color(0x88afef)
+		scene.background = new THREE.Color(0x000000)
 
 		camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000)
-		camera.position.z = 25
+		camera.position.set(0, 10, 20)
 
-		light = new THREE.AmbientLight(0xeeeeff)
+		// light = new THREE.AmbientLight(0x63b8ff)
+		light = new THREE.HemisphereLight(0xffffff, 0x000000)
+
 		scene.add(light)
 
 		createGeometry()
@@ -49,8 +48,6 @@ window.onload = () => {
 	}
 
 	let mainLoop = function() {
-		light.intensity += STEP
-		if (light.intensity >= 8 || light.intensity <= 1) STEP *= -1
 
 		renderer.render(scene, camera)
 		requestAnimationFrame(mainLoop)
