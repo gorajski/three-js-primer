62704d8345ba1ff1cac919d087dc024c45249e18
Nick Gorajski <nickgorajski@gmail.com>
2020-04-11 22:06:07 -0500

    Camera - Switch back and forth between PerspectiveCamera and OrthographicCamera

--- asset.js
+++ asset.js
@@ -1,41 +1,36 @@
 let scene, camera, renderer
-let cylinder, sphere, plane, light1
+let light1
+let spheres = []
 
 window.onload = () => { 
 	
-	let STEP = 0.1
+	let STEP = 0.01
 	let theta = 0
+	const RADIUS = 5, BASE_X = -20, BASE_Y = -20
 
 	let createGeometry = function() {
-		let geometry = new THREE.CylinderGeometry(5,5,20,32)
-		let material = new THREE.MeshPhongMaterial({color: 0x448844, shininess: 100, side: THREE.DoubleSide})
+		let material = new THREE.MeshPhongMaterial({color: 0x0450fb, shininess: 100, side: THREE.DoubleSide})
 
-		cylinder = new THREE.Mesh(geometry, material)
-		cylinder.position.set(6,0,-2)
+		for(let i = 0; i < 4; i++) {
+			for(let j = 0; j < 4; j++) {
+				let geometry = new THREE.SphereGeometry(RADIUS,30,30)
+				let sphere = new THREE.Mesh(geometry,material)
+				sphere.position.x = BASE_X + j * 2 * (RADIUS+0.5)
+				sphere.position.y = BASE_Y + i * RADIUS
+				sphere.position.z = -2*RADIUS * i
+				scene.add(sphere)
+			}
+		}
 
-		geometry = new THREE.SphereGeometry(5, 30, 30)
-		material = new THREE.MeshPhongMaterial({color: 0x693421, side: THREE.DoubleSide})
-
-		sphere = new THREE.Mesh(geometry,material)
-		sphere.position.set(-5, 5, 2)
-
-		geometry = new THREE.BoxGeometry(2000,1,2000)
-		material = new THREE.MeshPhongMaterial({color: 0xabcdef, side: THREE.DoubleSide})
-
-		plane = new THREE.Mesh(geometry, material)
-		plane.position.y = -1
-
-		scene.add(cylinder)
-		scene.add(sphere)
-		scene.add(plane)
 	}
+
 
 	let init = function() {
 		scene = new THREE.Scene()
-		scene.background = new THREE.Color(0x000000)
+		scene.background = new THREE.Color(0xffffff)
 
-		camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 7, 800)
-		camera.position.set(0,10,40)
+		camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 7, 1000)
+		camera.position.set(0,10,60)
 
 		light1 = new THREE.SpotLight(0xffffff, 1)
 		light1.position.set(0,10,15)
@@ -51,15 +46,25 @@ window.onload = () => {
 	}
 
 	let mainLoop = function() {
-		camera.lookAt(new THREE.Vector3(0,0,0))
-		camera.position.x = 40 * Math.sin(theta)
-		camera.position.z = 40 * Math.cos(theta)
-		theta += STEP
+
 
 		renderer.render(scene, camera)
 		requestAnimationFrame(mainLoop)
 	}
 
+	setInterval(switchCamera, 3000)
 	init()
 	mainLoop()
+}
+
+
+let switchCamera = function() {
+	if(camera instanceof THREE.PerspectiveCamera) {
+		camera = new THREE.OrthographicCamera(-300, 300, 400, -400, 1, 1000)
+		camera.zoom = 5
+		camera.updateProjectionMatrix()
+	} else {
+		camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000)
+		camera.position.set(0,0,40)
+	}
 }
