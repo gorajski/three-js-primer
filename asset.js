39cf8a08b7bca13b29bbfc1015a5b74b50441c02
Nick Gorajski <nickgorajski@gmail.com>
2020-04-11 21:29:17 -0500

    Camera - Oscillate field of view

--- asset.js
+++ asset.js
@@ -1,50 +1,48 @@
-let scene, camera, renderer, light, spotLight1, helper, spotLightTarget1
+let scene, camera, renderer
+let cylinder, sphere, plane, light1
 
 window.onload = () => { 
 	
-	let STEP = 0.33
+	let STEP = 0.1
 	let theta = 0
 
-	let createCube = function(size, color, position) {
-		let geometry = new THREE.BoxGeometry(...size)
-		let material = new THREE.MeshPhongMaterial({color: color, side: THREE.DoubleSide})
+	let createGeometry = function() {
+		let geometry = new THREE.CylinderGeometry(5,5,20,32)
+		let material = new THREE.MeshPhongMaterial({color: 0x448844, shininess: 100, side: THREE.DoubleSide})
 
-		let cube = new THREE.Mesh(geometry,material)
-		cube.position.set(...position)
+		cylinder = new THREE.Mesh(geometry, material)
+		cylinder.position.set(6,0,-2)
 
-		scene.add(cube)
+		geometry = new THREE.SphereGeometry(5, 30, 30)
+		material = new THREE.MeshPhongMaterial({color: 0x693421, side: THREE.DoubleSide})
+
+		sphere = new THREE.Mesh(geometry,material)
+		sphere.position.set(-5, 5, 2)
+
+		geometry = new THREE.BoxGeometry(2000,1,2000)
+		material = new THREE.MeshPhongMaterial({color: 0xabcdef, side: THREE.DoubleSide})
+
+		plane = new THREE.Mesh(geometry, material)
+		plane.position.y = -1
+
+		scene.add(cylinder)
+		scene.add(sphere)
+		scene.add(plane)
 	}
 
 	let init = function() {
 		scene = new THREE.Scene()
 		scene.background = new THREE.Color(0x000000)
 
-		camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000)
-		camera.position.set(0,7,14)
+		camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 7, 800)
+		camera.position.set(0,10,40)
 
-		spotLight1 = new THREE.SpotLight(0xffffff, 0.8, 200, Math.PI / 16, 0.075, 2)
-		spotLight1.position.set(15,20,0)
-		scene.add(spotLight1)
+		light1 = new THREE.SpotLight(0xffffff, 1)
+		light1.position.set(0,10,15)
 
-		spotLight2 = new THREE.SpotLight(0xffffff, 0.8, 200, Math.PI / 16, 0.075, 2)
-		spotLight2.position.set(-15,20,0)
-		scene.add(spotLight2)
+		scene.add(light1)
 
-		spotLightTarget1 = new THREE.Object3D()
-		scene.add(spotLightTarget1)
-		spotLight1.target = spotLightTarget1
-
-		spotLightTarget2 = new THREE.Object3D()
-		scene.add(spotLightTarget2)
-		spotLight2.target = spotLightTarget2
-
-		createCube([2000,1,2000], 0x693421, [0,0,0])
-		createCube([5,2,5], 0x33f933, [-2,1,-6])
-		createCube([5,3,5], 0xe36933, [-5,2,1])
-		createCube([5,3,5], 0x7329f3, [-10,1,-6])
-		createCube([5,3,5], 0xe32913, [8,0.5,2])
-		createCube([6,2,6], 0x0111f3, [7,1,-7])
-		createCube([6,2,6], 0x44444f, [1,0,6])
+		createGeometry()
 
 		renderer = new THREE.WebGLRenderer({antialias: true})
 		renderer.setSize(window.innerWidth, window.innerHeight)
@@ -53,11 +51,10 @@ window.onload = () => {
 	}
 
 	let mainLoop = function() {
-		
-		spotLightTarget1.position.x += STEP
-		spotLightTarget2.position.x -= STEP
-		if (spotLightTarget1.position.x > 20 || spotLightTarget1.position.x < -10) STEP *= -1
-
+		camera.fov += STEP
+		camera.updateProjectionMatrix()
+		if(camera.fov > 100 || camera.fov < 50)
+			STEP *= -1
 
 		renderer.render(scene, camera)
 		requestAnimationFrame(mainLoop)
