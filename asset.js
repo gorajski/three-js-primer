fb0b9a9187f5ed5ddc38f6fe57129ad871615293
Nick Gorajski <nickgorajski@gmail.com>
2020-04-12 14:04:50 -0500

    Interaction - Use Raycaster to highlight selected items in green

--- asset.js
+++ asset.js
@@ -1,6 +1,6 @@
 let scene, camera, renderer
-let light
-let cube
+let rayCast, mouse
+let cube, sphere
 
 window.onload = () => { 
 	
@@ -13,20 +13,32 @@ window.onload = () => {
 	}
 
 	let createGeometry = function() {
-		let sideLength = randomInRange(2,6)
-		let height = randomInRange(1,5)
-		let geometry = new THREE.BoxGeometry(sideLength, height, sideLength)
-		let material = new THREE.MeshPhongMaterial({color: Math.random() * 0xffffff, shininess: 100, side: THREE.DoubleSide})
+		let geometry = new THREE.BoxGeometry(3, 1, 3)
+		let material = new THREE.MeshPhongMaterial({color: 0xff2200, shininess: 100, side: THREE.DoubleSide})
 		cube = new THREE.Mesh(geometry,material)
-		cube.position.x = randomInRange(-5, 5)
-		// cube.position.y = height/2
-		cube.position.z = randomInRange(-25, -10)
-	
+		cube.position.x = 0
+		cube.position.y = -1.5
+		cube.position.z = -15
+		cube.rotation.set(0.1,-0.2,-0.1)
 		scene.add(cube)
+
+		geometry = new THREE.SphereGeometry(2, 20, 20)
+		material = new THREE.MeshPhongMaterial({color: 0x0036ee, shininess: 100, side: THREE.DoubleSide})
+		sphere = new THREE.Mesh(geometry,material)
+		sphere.position.x = 0
+		sphere.position.y = 0
+		sphere.position.z = -20
+		sphere.rotation.set(0.1,-0.2,-0.1)
+		scene.add(sphere)
 	}
 
-	let onMouseClick = function() {
-		STEP *= -1
+	let onMouseClick = function(e) {
+		mouse.x = (e.clientX / window.innerWidth) * 2 - 1
+		mouse.y = - (e.clientY / window.innerHeight) * 2 + 1
+		mouse.z = 1
+
+		rayCast.setFromCamera(mouse, camera)
+
 	}
 
 
@@ -37,10 +49,17 @@ window.onload = () => {
 		camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000)
 		camera.position.set(0,0,5)
 
-		light = new THREE.DirectionalLight(0xffffff, 1)
-		scene.add(light)
+		light1 = new THREE.DirectionalLight(0xffffff, 1)
+		light2 = new THREE.DirectionalLight(0xffffff, 0.75)
+		light2.position.set(0, -5, 2)
+		scene.add(light1)
+		scene.add(light2)
 
 		createGeometry()
+
+		rayCast = new THREE.Raycaster()
+		mouse = new THREE.Vector2()
+		mouse.x = mouse.y = -1
 
 		renderer = new THREE.WebGLRenderer({antialias: true})
 		renderer.setSize(window.innerWidth, window.innerHeight)
@@ -50,8 +69,11 @@ window.onload = () => {
 	}
 
 	let mainLoop = function() {
-		cube.rotation.x += STEP
-		cube.rotation.y += STEP
+		sphere.material.color.set(0x0036ee)
+		cube.material.color.set(0xff2200)
+
+		let intersects = rayCast.intersectObjects(scene.children)
+		intersects.forEach(obj => obj.object.material.color.set(0x00ff00))
 
 		renderer.render(scene, camera)
 		requestAnimationFrame(mainLoop)
