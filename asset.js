c68893fa894531c01d6998e773e06e98da7ab664
Nick Gorajski <nickgorajski@gmail.com>
2020-04-25 09:40:27 -0500

    Textures - wrap sphere and box with textures

--- asset.js
+++ asset.js
@@ -1,44 +1,34 @@
 let scene, camera, renderer
-let rayCast, mouse
-let box
+let box, sphere
 
 window.onload = () => { 
 	
-	let STEP = 0.2
-	let theta = 0
+	let STEP = 0.03
 
-	let randomInRange = function(from,to) {
-		let x = Math.random() * (to - from)
-		return x + from
-	}
+	// let randomInRange = function(from,to) {
+	// 	let x = Math.random() * (to - from)
+	// 	return x + from
+	// }
 
-	let createBox = function() {
-		let geometry = new THREE.BoxGeometry(2,0.6,2)
-		let material = new THREE.MeshPhongMaterial({color: 0x3344ff, shininess: 1})
+	let createGeometry = function() {
+		let texture = new THREE.TextureLoader().load('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTc8xREH3RMFHT9i58G87JY-a37q4g2JvIPAhgLxYLD8IDIE6V3')
+
+		// let material = new THREE.MeshBasicMaterial({map: texture})
+		let material = new THREE.MeshPhongMaterial({map: texture})
+		let geometry = new THREE.BoxGeometry(4,4,4)
 		box = new THREE.Mesh(geometry, material)
 
-		box.position.x = 0
-		box.position.y = -1
-		box.position.z = 7
-		box.rotation.x = 0.3
-		box.rotation.y = 0.4
 		scene.add(box)
+
+		texture = new THREE.TextureLoader().load('https://upload.wikimedia.org/wikipedia/commons/c/c1/Expanded_metal.jpg')
+		geometry = new THREE.SphereGeometry(4,30,30)
+		// material = new THREE.MeshBasicMaterial({map: texture})
+		material = new THREE.MeshPhongMaterial({map: texture})
+		sphere = new THREE.Mesh(geometry, material)
+		sphere.position.set(-7,2,-2)
+
+		scene.add(sphere)
 	}
-
-	let onMouseMove = function(e) {
-		mouse.x = (2 * e.clientX / window.innerWidth) - 1
-		mouse.y = (-2 * e.clientY / window.innerHeight) + 1
-
-		rayCast.setFromCamera(mouse, camera)
-		
-		boxDistance = camera.position.z - box.position.z
-		
-		pointer = rayCast.ray.at(boxDistance, new THREE.Vector3(0,0,0))
-		
-		box.position.x = pointer.x
-		box.position.y = pointer.y
-	}
-
 
 	let init = function() {
 		scene = new THREE.Scene()
@@ -47,28 +37,26 @@ window.onload = () => {
 		camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000)
 		camera.position.set(0,0,20)
 
-		light1 = new THREE.DirectionalLight(0xffffff, 1)
-		light2 = new THREE.DirectionalLight(0xffffff, 0.75)
-		light2.position.set(0, -5, -2)
-		scene.add(light1)
-		scene.add(light2)
+		let directionalLightUp = new THREE.DirectionalLight(0xffffff)
+		scene.add(directionalLightUp)
 
-		createBox()
+		let directionalLightDown = new THREE.DirectionalLight(0xabcdef)
+		scene.add(directionalLightDown)
+		directionalLightDown.position.y = -1
 
-		rayCast = new THREE.Raycaster()
-
-		mouse = new THREE.Vector2()
-		mouse.x = mouse.y = -1
+		createGeometry()
 
 		renderer = new THREE.WebGLRenderer({antialias: true})
 		renderer.setSize(window.innerWidth, window.innerHeight)
 
 		document.body.appendChild(renderer.domElement)
-		document.addEventListener("mousemove", onMouseMove)
 	}
 
 	let mainLoop = function() {
-		
+		box.rotation.x += STEP
+		box.rotation.y += STEP
+		sphere.rotation.x += STEP
+		sphere.rotation.y += STEP
 
 		renderer.render(scene, camera)
 		requestAnimationFrame(mainLoop)
