63e4211468e56fce254d1342891d77f8bb60facb
Nick Gorajski <nickgorajski@gmail.com>
2020-04-28 18:28:59 -0500

    Textures - wrap texture inside sphere

--- asset.js
+++ asset.js
@@ -1,9 +1,10 @@
 let scene, camera, renderer
-let box, sphere
+let sphere, target, texture
 
 window.onload = () => { 
 	
-	let STEP = 0.03
+	let STEP = 0.02
+	let theta = 0
 
 	// let randomInRange = function(from,to) {
 	// 	let x = Math.random() * (to - from)
@@ -11,21 +12,15 @@ window.onload = () => {
 	// }
 
 	let createGeometry = function() {
-		let texture = new THREE.TextureLoader().load('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTc8xREH3RMFHT9i58G87JY-a37q4g2JvIPAhgLxYLD8IDIE6V3')
+		texture = new THREE.TextureLoader().load('https://upload.wikimedia.org/wikipedia/commons/2/26/Clouds_from_the_sky.jpg')
+		texture.wrapS = THREE.RepeatWrapping
+		texture.wrapT = THREE.RepeatWrapping
+		texture.repeat.set(2,2)
 
-		// let material = new THREE.MeshBasicMaterial({map: texture})
-		let material = new THREE.MeshPhongMaterial({map: texture})
-		let geometry = new THREE.BoxGeometry(4,4,4)
-		box = new THREE.Mesh(geometry, material)
+		geometry = new THREE.SphereGeometry(5, 100, 100)
+		material = new THREE.MeshBasicMaterial({map: texture, side: THREE.DoubleSide})
 
-		scene.add(box)
-
-		texture = new THREE.TextureLoader().load('https://upload.wikimedia.org/wikipedia/commons/c/c1/Expanded_metal.jpg')
-		geometry = new THREE.SphereGeometry(4,30,30)
-		// material = new THREE.MeshBasicMaterial({map: texture})
-		material = new THREE.MeshPhongMaterial({map: texture})
 		sphere = new THREE.Mesh(geometry, material)
-		sphere.position.set(-7,2,-2)
 
 		scene.add(sphere)
 	}
@@ -35,14 +30,15 @@ window.onload = () => {
 		scene.background = new THREE.Color(0xffffff)
 
 		camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000)
-		camera.position.set(0,0,20)
 
-		let directionalLightUp = new THREE.DirectionalLight(0xffffff)
-		scene.add(directionalLightUp)
+		target = new THREE.Object3D()
+		camera.lookAt(target.position)
+		// let directionalLightUp = new THREE.DirectionalLight(0xffffff)
+		// scene.add(directionalLightUp)
 
-		let directionalLightDown = new THREE.DirectionalLight(0xabcdef)
-		scene.add(directionalLightDown)
-		directionalLightDown.position.y = -1
+		// let directionalLightDown = new THREE.DirectionalLight(0xabcdef)
+		// scene.add(directionalLightDown)
+		// directionalLightDown.position.y = -1
 
 		createGeometry()
 
@@ -53,11 +49,10 @@ window.onload = () => {
 	}
 
 	let mainLoop = function() {
-		box.rotation.x += STEP
-		box.rotation.y += STEP
-		sphere.rotation.x += STEP
-		sphere.rotation.y += STEP
-
+		target.position.x = 10 * Math.sin(theta)
+		target.position.z = 10 * Math.cos(theta)
+		theta += STEP
+		camera.lookAt(target.position)
 		renderer.render(scene, camera)
 		requestAnimationFrame(mainLoop)
 	}
