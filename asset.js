933404bb1a3a8fb144d7d6c45ec119ddee8c65bd
Nick Gorajski <nickgorajski@gmail.com>
2020-04-28 19:39:19 -0500

    Shadows - Create scene with rotating shadows - problem with one cube

--- asset.js
+++ asset.js
@@ -1,5 +1,5 @@
 let scene, camera, renderer
-let sphere, target, texture
+let cube1, cube2, spotLight, plane
 
 window.onload = () => { 
 	
@@ -12,47 +12,74 @@ window.onload = () => {
 	// }
 
 	let createGeometry = function() {
-		texture = new THREE.TextureLoader().load('https://upload.wikimedia.org/wikipedia/commons/2/26/Clouds_from_the_sky.jpg')
-		texture.wrapS = THREE.RepeatWrapping
-		texture.wrapT = THREE.RepeatWrapping
-		texture.repeat.set(2,2)
+		
+		let geometry = new THREE.BoxGeometry(5, 5, 5)
+		let material = new THREE.MeshPhongMaterial({color: 0xdff913, shininess: 100, side: THREE.DoubleSide})
 
-		geometry = new THREE.SphereGeometry(5, 100, 100)
-		material = new THREE.MeshBasicMaterial({map: texture, side: THREE.DoubleSide})
+		cube1 = new THREE.Mesh(geometry, material)
+		cube1.position.set(5, 2, 0)
+		cube1.castShadow = true
+		cube1.receiveShadow = true
 
-		sphere = new THREE.Mesh(geometry, material)
+		geometry = new THREE.BoxGeometry(5, 6, 4)
+		cube2 = new THREE.Mesh(geometry, material)
+		cube2.position.set(-4, 2, 0)
+		cube2.castShadow = true
 
-		scene.add(sphere)
+		geometry = new THREE.BoxGeometry(2000,1,2000)
+		material = new THREE.MeshPhongMaterial({color: 0x693421, side: THREE.DoubleSide})
+		plane = new THREE.Mesh(geometry,material)
+		plane.position.y = -1
+		plane.receiveShadow = true
+
+		scene.add(cube1)
+		scene.add(cube2)
+		scene.add(plane)
 	}
 
 	let init = function() {
 		scene = new THREE.Scene()
-		scene.background = new THREE.Color(0xffffff)
+		scene.background = new THREE.Color(0x000000)
+		scene.fog = new THREE.Fog(0x000000)
 
 		camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000)
-
-		target = new THREE.Object3D()
-		camera.lookAt(target.position)
-		// let directionalLightUp = new THREE.DirectionalLight(0xffffff)
-		// scene.add(directionalLightUp)
-
-		// let directionalLightDown = new THREE.DirectionalLight(0xabcdef)
-		// scene.add(directionalLightDown)
-		// directionalLightDown.position.y = -1
+		camera.position.set(0,5,40)
 
 		createGeometry()
+		spotLight = new THREE.SpotLight(0xffffff, 1)
+		spotLight.position.set(0,15,10)
+		spotLight.angle = Math.PI / 2
+		spotLight.penumbra = 0.05
+		spotLight.decay = 2
+		spotLight.distance = 200
 
-		renderer = new THREE.WebGLRenderer({antialias: true})
+		spotLight.castShadow = true
+		spotLight.shadow = new THREE.LightShadow(new THREE.PerspectiveCamera(50, 1, 10, 2500))
+		spotLight.shadow.bias = 0.0001
+		spotLight.shadow.mapSize.width = 2048
+		spotLight.shadow.mapSize.height = 1024
+
+		let shadowCameraHelper = new THREE.CameraHelper(spotLight.shadow.camera);
+		shadowCameraHelper.visible = true;
+		scene.add(shadowCameraHelper)
+
+		scene.add(spotLight)
+
+
+		renderer = new THREE.WebGLRenderer()
 		renderer.setSize(window.innerWidth, window.innerHeight)
+
+		renderer.shadowMap.enabled = true
+		renderer.shadowMap.type = THREE.PCFShadowMap
 
 		document.body.appendChild(renderer.domElement)
 	}
 
 	let mainLoop = function() {
-		target.position.x = 10 * Math.sin(theta)
-		target.position.z = 10 * Math.cos(theta)
+		spotLight.position.x = 10 * Math.sin(theta)
+		spotLight.position.z = 10 * Math.cos(theta)
 		theta += STEP
-		camera.lookAt(target.position)
+
 		renderer.render(scene, camera)
 		requestAnimationFrame(mainLoop)
 	}
