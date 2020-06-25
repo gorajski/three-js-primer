f63ec7b633fc19060500287a843b8d174032d9ee
Nick Gorajski <nickgorajski@gmail.com>
2020-06-24 22:29:19 -0500

    Creates scene of pyramids at sunrise/sunset

--- asset.js
+++ asset.js
@@ -1,63 +1,80 @@
 let scene, camera, renderer
-let cube1, cube2, spotLight, plane
+let pyramid1, pyramid2, pyramid3, spotLight, plane
 
 window.onload = () => { 
 	
-	let STEP = 0.02
+	let STEP = 0.01
 	let theta = 0
 
-	// let randomInRange = function(from,to) {
-	// 	let x = Math.random() * (to - from)
-	// 	return x + from
-	// }
+	let randomInRange = function(from,to) {
+		let x = Math.random() * (to - from)
+		return x + from
+	}
 
 	let createGeometry = function() {
+		let pyramidTexture = new THREE.TextureLoader().load('https://as2.ftcdn.net/jpg/02/08/05/15/500_F_208051513_sRobBVhkSgvYLxo6pmCavAxwjONTKHT2.jpg')
+		let groundTexture = new THREE.TextureLoader().load('https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/016vallesmarineris_reduced0.25.jpg/1024px-016vallesmarineris_reduced0.25.jpg')
 		
-		let geometry = new THREE.BoxGeometry(5, 5, 5)
-		let material = new THREE.MeshPhongMaterial({color: 0xdff913, shininess: 100, side: THREE.DoubleSide})
+		let geometry = new THREE.CylinderBufferGeometry(0, 11, 11, 4)
+		let material = new THREE.MeshPhongMaterial({ map: pyramidTexture, shininess: 12, side: THREE.DoubleSide })
 
-		cube1 = new THREE.Mesh(geometry, material)
-		cube1.position.set(5, 2, 0)
-		cube1.castShadow = true
-		cube1.receiveShadow = true
+		pyramid1 = new THREE.Mesh(geometry, material)
+		pyramid1.position.set(20, 3.75, -30)
+		pyramid1.castShadow = true
+		pyramid1.receiveShadow = false
 
-		geometry = new THREE.BoxGeometry(5, 6, 4)
-		cube2 = new THREE.Mesh(geometry, material)
-		cube2.position.set(-4, 2, 0)
-		cube2.castShadow = true
+		geometry = new THREE.CylinderBufferGeometry(0, 10, 10, 4)
+		pyramid2 = new THREE.Mesh(geometry, material)
+		pyramid2.position.set(-10, 3, 4)
+		pyramid2.castShadow = true
+
+		geometry = new THREE.CylinderBufferGeometry(0, 15, 15, 4)
+		pyramid3 = new THREE.Mesh(geometry, material)
+		pyramid3.position.set(0, 7, -13)
+		pyramid3.castShadow = true
+		pyramid3.receiveShadow = false
+
+		geometry = new THREE.CylinderBufferGeometry(0, 4, 4, 4)
+		pyramid4 = new THREE.Mesh(geometry, material)
+		pyramid4.position.set(-22, 1, -13)
+		pyramid4.castShadow = true
+		pyramid4.receiveShadow = false
 
 		geometry = new THREE.BoxGeometry(2000,1,2000)
-		material = new THREE.MeshPhongMaterial({color: 0x693421, side: THREE.DoubleSide})
+		material = new THREE.MeshPhongMaterial({map: groundTexture, shininess: 20, side: THREE.DoubleSide})
 		plane = new THREE.Mesh(geometry,material)
 		plane.position.y = -1
 		plane.receiveShadow = true
 
-		scene.add(cube1)
-		scene.add(cube2)
+		scene.add(pyramid1)
+		scene.add(pyramid2)
+		scene.add(pyramid3)
+		scene.add(pyramid4)
 		scene.add(plane)
 	}
 
 	let init = function() {
 		scene = new THREE.Scene()
-		scene.background = new THREE.Color(0x000000)
+		scene.background = new THREE.Color(0xaaccff)
 		scene.fog = new THREE.Fog(0x000000)
 
 		camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000)
-		camera.position.set(0,5,40)
+		camera.position.set(0,12,66)
 
 		createGeometry()
 		spotLight = new THREE.SpotLight(0xffffff, 1)
-		spotLight.position.set(0,15,10)
+		spotLight.position.set(0,500,10)
 		spotLight.angle = Math.PI / 2
 		spotLight.penumbra = 0.05
-		spotLight.decay = 2
+		spotLight.decay = 0
 		spotLight.distance = 200
+		spotLight.intensity = 1.8
 
 		spotLight.castShadow = true
 		spotLight.shadow = new THREE.LightShadow(new THREE.PerspectiveCamera(50, 1, 10, 2500))
 		spotLight.shadow.bias = 0.0001
 		spotLight.shadow.mapSize.width = 2048
-		spotLight.shadow.mapSize.height = 1024
+		spotLight.shadow.mapSize.height = 2048
 
 		let shadowCameraHelper = new THREE.CameraHelper(spotLight.shadow.camera);
 		shadowCameraHelper.visible = true;
@@ -76,8 +93,8 @@ window.onload = () => {
 	}
 
 	let mainLoop = function() {
-		spotLight.position.x = 10 * Math.sin(theta)
-		spotLight.position.z = 10 * Math.cos(theta)
+		spotLight.position.x = 800 * Math.sin(theta)
+		spotLight.position.y = 800 * Math.cos(theta)
 		theta += STEP
 
 		renderer.render(scene, camera)
