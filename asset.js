b1089cc6dbadbf17ab8a53ea93bc9b5c55f00b5c
Nick Gorajski <nickgorajski@gmail.com>
2020-10-10 16:46:28 -0500

    Create scene of pyramids with alternative solution

--- asset.js
+++ asset.js
@@ -1,100 +1,65 @@
-let scene, camera, renderer
-let pyramid1, pyramid2, pyramid3, spotLight, plane
+let scene, camera, renderer, light, plane
+let STEP = 0.02, theta = 0
 
-window.onload = () => { 
-	
-	let STEP = 0.01
-	let theta = 0
-
-	let randomInRange = function(from,to) {
-		let x = Math.random() * (to - from)
-		return x + from
-	}
+window.onload = function() {
 
 	let createGeometry = function() {
-		let pyramidTexture = new THREE.TextureLoader().load('https://as2.ftcdn.net/jpg/02/08/05/15/500_F_208051513_sRobBVhkSgvYLxo6pmCavAxwjONTKHT2.jpg')
-		let groundTexture = new THREE.TextureLoader().load('https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/016vallesmarineris_reduced0.25.jpg/1024px-016vallesmarineris_reduced0.25.jpg')
-		
-		let geometry = new THREE.CylinderBufferGeometry(0, 11, 11, 4)
-		let material = new THREE.MeshPhongMaterial({ map: pyramidTexture, shininess: 12, side: THREE.DoubleSide })
-
-		pyramid1 = new THREE.Mesh(geometry, material)
-		pyramid1.position.set(20, 3.75, -30)
-		pyramid1.castShadow = true
-		pyramid1.receiveShadow = false
-
-		geometry = new THREE.CylinderBufferGeometry(0, 10, 10, 4)
-		pyramid2 = new THREE.Mesh(geometry, material)
-		pyramid2.position.set(-10, 3, 4)
-		pyramid2.castShadow = true
-
-		geometry = new THREE.CylinderBufferGeometry(0, 15, 15, 4)
-		pyramid3 = new THREE.Mesh(geometry, material)
-		pyramid3.position.set(0, 7, -13)
-		pyramid3.castShadow = true
-		pyramid3.receiveShadow = false
-
-		geometry = new THREE.CylinderBufferGeometry(0, 4, 4, 4)
-		pyramid4 = new THREE.Mesh(geometry, material)
-		pyramid4.position.set(-22, 1, -13)
-		pyramid4.castShadow = true
-		pyramid4.receiveShadow = false
-
-		geometry = new THREE.BoxGeometry(2000,1,2000)
-		material = new THREE.MeshPhongMaterial({map: groundTexture, shininess: 20, side: THREE.DoubleSide})
-		plane = new THREE.Mesh(geometry,material)
+		let texture = new THREE.TextureLoader().load('https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/Adrar_sands.JPG/1280px-Adrar_sands.JPG')
+		let material = new THREE.MeshLambertMaterial({map: texture})
+		let geometry = new THREE.BoxGeometry(1000, 1, 1000)
+		plane = new THREE.Mesh(geometry, material)
 		plane.position.y = -1
 		plane.receiveShadow = true
 
-		scene.add(pyramid1)
-		scene.add(pyramid2)
-		scene.add(pyramid3)
-		scene.add(pyramid4)
 		scene.add(plane)
+		scene.add(createPyramid(115, 9, 0, 20, 25))
+		scene.add(createPyramid(50, 14, -20, 30, 40))
+		scene.add(createPyramid(-20, 9, 0, 20, 25))
+		scene.add(createPyramid(-60, 4, -35, 10, 10))
+	}
+
+	let createPyramid = function(x, y, z, height, width) {
+		let texture = new THREE.TextureLoader().load('https://upload.wikimedia.org/wikipedia/commons/3/3b/Tuff_ohyaishi02.jpg')
+		let geometry = new THREE.CylinderGeometry(0, width, height, 4)
+		let material = new THREE.MeshLambertMaterial({map: texture})
+		let p = new THREE.Mesh(geometry, material)
+		p.position.set(x, y, z)
+		p.castShadow = true
+		p.receiveShadow = true
+		return p
 	}
 
 	let init = function() {
 		scene = new THREE.Scene()
-		scene.background = new THREE.Color(0xaaccff)
-		scene.fog = new THREE.Fog(0x000000)
+		scene.background = new THREE.Color(0xbbddff)
 
 		camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000)
-		camera.position.set(0,12,66)
+		camera.position.set(0, 7, 90)
+		camera.lookAt(new THREE.Vector3(-60,4,-35))
 
 		createGeometry()
-		spotLight = new THREE.SpotLight(0xffffff, 1)
-		spotLight.position.set(0,500,10)
-		spotLight.angle = Math.PI / 2
-		spotLight.penumbra = 0.05
-		spotLight.decay = 0
-		spotLight.distance = 200
-		spotLight.intensity = 1.8
 
-		spotLight.castShadow = true
-		spotLight.shadow = new THREE.LightShadow(new THREE.PerspectiveCamera(50, 1, 10, 2500))
-		spotLight.shadow.bias = 0.0001
-		spotLight.shadow.mapSize.width = 2048
-		spotLight.shadow.mapSize.height = 2048
+		light = new THREE.DirectionalLight(0xffffff, 1)
+		light.castShadow = true
 
-		let shadowCameraHelper = new THREE.CameraHelper(spotLight.shadow.camera);
-		shadowCameraHelper.visible = true;
-		scene.add(shadowCameraHelper)
+		light.shadow = new THREE.LightShadow(new THREE.PerspectiveCamera(60, 2, 10, 2000))
+		light.shadow.bias = 0.00001
+		light.shadow.mapSize.width = 2048
+		light.shadow.mapSize.height = 1024
+		light.position.set(180, 90, 0)
 
-		scene.add(spotLight)
-
+		scene.add(light)
 
 		renderer = new THREE.WebGLRenderer()
-		renderer.setSize(window.innerWidth, window.innerHeight)
-
 		renderer.shadowMap.enabled = true
-		renderer.shadowMap.type = THREE.PCFShadowMap
-
+		renderer.setSize(window.innerWidth, window.innerHeight)
+		
 		document.body.appendChild(renderer.domElement)
 	}
 
 	let mainLoop = function() {
-		spotLight.position.x = 800 * Math.sin(theta)
-		spotLight.position.y = 800 * Math.cos(theta)
+		light.position.x = 200 * Math.cos(theta)
+		light.position.y = 200 * Math.sin(theta)
 		theta += STEP
 
 		renderer.render(scene, camera)
@@ -103,4 +68,5 @@ window.onload = () => {
 
 	init()
 	mainLoop()
+
 }
