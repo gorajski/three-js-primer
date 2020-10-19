282e5d1094b2e6216751ec989e0641c439f2d773
Nick Gorajski <nickgorajski@gmail.com>
2020-10-18 19:18:48 -0500

    Orbit particles around central light source - alternate implementation

--- asset.js
+++ asset.js
@@ -1,62 +1,79 @@
-let scene, camera, renderer, light, particles = []
-let STEP = 0.08, theta = 0
+let scene, camera, renderer, particles = []
+let STEP = 0.2, theta = 0
 const LEFT = 37, RIGHT = 39, UP = 38, DOWN = 40
 
 window.onload = function() {
 
-	let createGeometry = function() {
-
-		[...Array(10)].map(() => {
-		  
-			let radius = 20*Math.random()+1
-			let position = new THREE.Vector3(0,0,radius)
-			let frequency = Math.random()
-			let phase = new THREE.Vector3(20*Math.random(), 20*Math.random(), 20*Math.random())
-
-			createParticle(position, radius, frequency, phase)
-		});
+	let randomInRange = (from, to) => {
+		let x = Math.random() * (to - from)
+		return x + from
 	}
 
-	let createParticle = function(position, radius, frequency, phase) {
-		let geometry = new THREE.SphereGeometry(1, 15, 13)
-		let material = new THREE.MeshPhongMaterial({side: THREE.DoubleSide, color: 0xf3f3f3, specular: 0xffffff, shininess: 8})
-		let particle = new THREE.Mesh(geometry, material)
+	class Particle {
+		constructor() {
+			let geometry = new THREE.SphereGeometry(0.5, 30, 30)
+			let material = new THREE.MeshPhongMaterial(
+				{color: 0xffffff, shininess: 100, specular: 0xafeeee, side: THREE.DoubleSide}
+			)
+			this.mesh = new THREE.Mesh(geometry, material)
+			this.mesh.position.x = randomInRange(-15,15)
+			this.mesh.position.y = randomInRange(-5,5)
+			this.mesh.position.z = randomInRange(-10,10)
+			this.radius = this.mesh.position.x
 
-		particle.position.set(position)
-		particle.radius = radius
-		particle.frequency = frequency
-		particle.phase = phase
+			this.theta = 0
+			this.dTheta = 2 * Math.PI / randomInRange(150, 200)
+			this.yFactor = randomInRange(0, Math.PI)
+		}
 
-		particles.push(particle)
+		move() {
+			this.mesh.position.x = this.radius * Math.sin(this.theta)
+			this.mesh.position.y = this.radius * Math.sin(this.theta + this.yFactor)
+			this.mesh.position.z = this.radius * Math.cos(this.theta)
+			this.theta += this.dTheta
+		}
 	}
 
-	let createLight = function() {
-		light = new THREE.PointLight(0xffffff, 1, 50)
-		light.position.set(0,0,0)
-		scene.add(light)
+	let createGeometry = () => {
+		for(let i = 1; i <= 10; i++) {
+			let p = new Particle()
+			particles.push(p)
+			scene.add(p.mesh)
+		}
+	}
+
+	let createLights = function() {
+		let light1 = new THREE.PointLight(0xffffff, 2, 30, 2)
+		let light2 = new THREE.PointLight(0xffffff, 2, 30, 2)
+		let light3 = new THREE.PointLight(0xffffff, 2, 30, 2)
+		light1.position.set(0,0,0)
+		light2.position.set(0,10,0)
+		light3.position.set(0,-10,0)
+		scene.add(light1)
+		scene.add(light2)
+		scene.add(light3)
 	}
 
 	let onKeyDown = function(e) {
 		if (e.keyCode == DOWN) {
-			camera.position.z += 10*STEP
+			camera.position.z += 1*STEP
 		} else if (e.keyCode == UP) {
-			camera.position.z -= 10*STEP
+			camera.position.z -= 1*STEP
 		}
 	}
 
 	let init = function() {
 		scene = new THREE.Scene()
-		scene.background = new THREE.Color(0x0a0a0a)
+		scene.background = new THREE.Color(0x000000)
 
-		camera = new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight)
-		camera.position.set(0, 0, 50)
+		camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 1, 1000)
+		camera.position.set(0, 0, 20)
 		scene.add(camera)
 
-		createLight()
+		createLights()
 		createGeometry()
 
 		renderer = new THREE.WebGLRenderer()
-		renderer.shadowMap.enabled = true
 		renderer.setSize(window.innerWidth, window.innerHeight)
 
 		document.body.appendChild(renderer.domElement)
@@ -64,15 +81,7 @@ window.onload = function() {
 	}
 
 	let mainLoop = function() {
-		theta += STEP
-
-		for(let particle of particles) {
-			particle.position.x = particle.radius * Math.sin(particle.frequency * theta + particle.phase.x)
-			particle.position.y = particle.radius * Math.sin(particle.frequency * theta + particle.phase.y)
-			particle.position.z = particle.radius * Math.sin(particle.frequency * theta + particle.phase.z)
-
-			scene.add(particle)
-		}
+		particles.forEach(p => p.move())
 
 		renderer.render(scene, camera)
 		requestAnimationFrame(mainLoop)
